e7019a944f74ae32d8f88deec2fb7c1e5fcbe7d6
el19004 <62252947+lamprosfra@users.noreply.github.com>
2022-06-04 12:42:44 +0300

    Add files via upload

---
+++ indexes.sql
@@ -0,0 +1,7 @@
+                    
+
+
+create index idx_proj_stelexos on project(ManagerID);
+create index idx_researcher_organization on researcher(NameOrg);
+                               
+                         
